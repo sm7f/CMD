fb227ac3925f6920d2d3dec35523d1b3fd4dc750
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2023-05-25 09:03:59 -0300

    .

---
+++ ADMdefinitivo.bat
@@ -0,0 +1,1 @@
+REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /V EnableLUA /T REG_DWORD /D 0 /F
---
+++ PreRequisitosPermissôes.bat
@@ -0,0 +1,11 @@
+icacls "C:\Maqplan" /grant "Todos":(OI)(CI)F /t /c
+
+icacls "C:\MaqplanNFe" /grant "Todos":(OI)(CI)F /t /c
+
+netsh advfirewall set allprofiles state off
+
+
+
+
+
+
