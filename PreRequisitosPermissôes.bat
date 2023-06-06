6ada200a83ee3def6948de6d9b7be02b8f41dd28
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2023-06-06 10:26:32 -0300

    .

--- PreRequisitosPermissôes.bat
+++ PreRequisitosPermissôes.bat
@@ -1,5 +1,7 @@
 icacls "C:\Maqplan" /grant "Todos":(OI)(CI)F /t /c
 
+icacls "C:\MSoftware" /grant "Todos":(OI)(CI)F /t /c
+
 icacls "C:\MaqplanNFe" /grant "Todos":(OI)(CI)F /t /c
 
 netsh advfirewall set allprofiles state off
