8f4aff305b2a27f7e535043b38ac1e131c1f5085
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2024-04-19 10:00:04 -0300

    .

--- PreRequisitosPermissôes.bat
+++ PreRequisitosPermissôes.bat
@@ -5,3 +5,10 @@ icacls "C:\MSoftware" /grant "Todos":(OI)(CI)F /t /c
 icacls "C:\MaqplanNFe" /grant "Todos":(OI)(CI)F /t /c
 
 netsh advfirewall set allprofiles state off
+
+REG ADD "HKCU\SOFTWARE\VB and VBA Program Settings\Psylicn\Controle" /v CdEmpCntCtr /d 4651
+
+
+DESKTOP-G5ACJ05
+
+034696000108
