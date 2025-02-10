6b33e9ce8f78c3b222f05f0486b58fc2d37e2722
Herberth Amorim <86411631+sm7f@users.noreply.github.com>
2025-02-10 11:46:05 -0300

    .

--- mudar-idioma-win-10-PT-br/Comandos.bat
+++ mudar-idioma-win-10-PT-br/Comandos.bat
@@ -1,21 +1,21 @@
-Abrir no PowerShell:
+"Abrir no PowerShell:"
 DISM /Online /Add-Capability /CapabilityName:Language.Basic~~~pt-BR~0.0.1.0
 
-Opcional:
+"Opcional:"
 DISM /Online /Add-Capability /CapabilityName:Language.Handwriting~~~pt-BR~0.0.1.0
 DISM /Online /Add-Capability /CapabilityName:Language.OCR~~~pt-BR~0.0.1.0
 DISM /Online /Add-Capability /CapabilityName:Language.Speech~~~pt-BR~0.0.1.0
 
-Verificar se está disponivel: Abra o CMD(Administrador) Execute:
+"Verificar se está disponivel: Abra o CMD(Administrador) Execute:"
 DISM /Online /Get-Capabilities | findstr "Language"
 
-Para confirmar se Português (Brasil) está instalado, use:
+"Para confirmar se Português (Brasil) está instalado, use:"
 DISM /Online /Get-Capabilities | findstr "pt-BR"
 
-Verificar o idioma atual do sistema (PowerShell):
+"Verificar o idioma atual do sistema (PowerShell):
 Get-WinUserLanguageList
   
-Outra opção:
+"Outra opção:
 Get-WinUILanguageOverride
 
 (Powershell)
@@ -29,3 +29,7 @@ Reinicar o computador após executar os comandos.
 Caso não mude, vai em pesquisar, digite "Language" ira aparecer as configurações de linguagem, dessa forma você define a linguagem PT-BR
 Após deixar tudo em PT-BR reinicie o computador.
 
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+\               BY HERBERTH AMORIM                 \
+\               github.com/sm7f                    \
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
